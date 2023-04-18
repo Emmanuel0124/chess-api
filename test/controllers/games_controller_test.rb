629bd9bb27554f344abce574183dc5f32d8eb6c0
EmmanuelBahena <mannybahena04@gmail.com>
2023-04-18 18:33:54 -0500

    add test for create action

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -8,4 +8,12 @@ class GamesControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Game.count, data.length
   end
+
+  test "create" do
+    assert_difference "Game.count", 1 do
+      post "/games.json", params: { name: "lake", width: 800, height: 600 }
+      assert_response 200
+    end
+  end
+
 end
