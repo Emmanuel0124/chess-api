208fb70f34d940a3e442e371d213a42708d6eec8
EmmanuelBahena <mannybahena04@gmail.com>
2023-04-18 18:30:54 -0500

    add create action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,4 +3,14 @@ class GamesController < ApplicationController
     @games = Game.all
     render :index
   end 
+  def create 
+    @game = Game.create(
+      winner: params[:winner],
+      ending: params[:ending],
+      pgn: params[:pgn],
+      pgn_image: params[:pgn_image]
+    )
+    render :show
+
+  end 
 end
