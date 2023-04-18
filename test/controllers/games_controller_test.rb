51c25cbec545e4f0d1497ae0c8f44b832f9c3408
EmmanuelBahena <mannybahena04@gmail.com>
2023-04-18 18:17:11 -0500

    add test for index action

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class GamesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/games.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Game.count, data.length
+  end
 end
