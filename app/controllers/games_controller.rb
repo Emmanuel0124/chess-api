c593cd95715686f1c4dd08b6f078688a587876da
EmmanuelBahena <mannybahena04@gmail.com>
2023-04-24 15:24:34 -0500

    add code to download pgn as a file

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,29 +5,29 @@ class GamesController < ApplicationController
     web_request = HTTP.get("https://api.chess.com/pub/player/manny7100/games/2023/04").parse(:json)
     render json: web_request
 
-    require 'net/http'
-    username = "manny7100"
-    year = "2023"
-    month = "04"
-    url = "https://api.chess.com/pub/player/manny7100/games/2023/4/pgn"
-    uri = URI(url)
-    response = Net::HTTP.get_response(uri)
+    # require 'net/http'
+    # username = "manny7100"
+    # year = "2023"
+    # month = "04"
+    # url = "https://api.chess.com/pub/player/manny7100/games/2023/4/pgn"
+    # uri = URI(url)
+    # response = Net::HTTP.get_response(uri)
     
-    if response.code == "200"
-      content_type = response["Content-Type"]
-      content_disposition = response["Content-Disposition"]
-      suggested_filename = content_disposition.match(/filename="(.+)"/)[1]
+    # if response.code == "200"
+    #   content_type = response["Content-Type"]
+    #   content_disposition = response["Content-Disposition"]
+    #   suggested_filename = content_disposition.match(/filename="(.+)"/)[1]
       
-      pgn_data = response.body
+    #   pgn_data = response.body
       
-      File.open(suggested_filename, "w") do |file|
-        file.write(pgn_data)
-      end 
+    #   File.open(suggested_filename, "w") do |file|
+    #     file.write(pgn_data)
+    #   end 
       
-      puts "PGN file downloaded and saved as #{suggested_filename}"
-    else
-      puts "Error: #{response.code} - #{response.message}"
-    end 
+    #   puts "PGN file downloaded and saved as #{suggested_filename}"
+    # else
+    #   puts "Error: #{response.code} - #{response.message}"
+    # end 
   end 
 
   def create 
@@ -40,6 +40,7 @@ class GamesController < ApplicationController
     render :show
 
   end 
+  
   def show 
     @game = Game.find_by(id: params[:id])
     render :show 
