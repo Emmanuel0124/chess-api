dc2bfa91a3982877146284847c751f1f3208bf17
EmmanuelBahena <mannybahena04@gmail.com>
2023-04-25 15:59:10 -0500

    edit users create action to be sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,17 @@ class UsersController < ApplicationController
     render :index 
   end
   def create
-    @user = User.create(
+    user = User.new(
       name: params[:name],
       email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
     )
-    render :show
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
   end
   def update
     @user = User.find_by(id: params[:id])
