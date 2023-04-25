da908974eb01c9f3b20decbf37413956dc1d22bf
EmmanuelBahena <mannybahena04@gmail.com>
2023-04-25 15:11:58 -0500

    add all routes with recources shortcut

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get "/users" => "users#index"
-  post "/users" => "users#create"
-  patch "/users/:id" => "users#update"
+  resources :users
+  resources :games
 
-  get "/games" => "games#index"
-  get "/games/:id" => "games#show"
-  post "/games" => "games#create"
-  patch "/games/:id" => "games#update"
+  # get "/users" => "users#index"
+  # post "/users" => "users#create"
+  # patch "/users/:id" => "users#update"
+
+  # get "/games" => "games#index"
+  # get "/games/:id" => "games#show"
+  # post "/games" => "games#create"
+  # patch "/games/:id" => "games#update"
 
 end 
